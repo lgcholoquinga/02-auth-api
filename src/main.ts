d92d39b738375800c91c9b6596fff40dba0852b3
lgcholoquinga <luisg.choloquinga@gamil.com>
2024-08-23 22:28:03 -0500

    Clean project more generate docker mongo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('Auth-Api');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+
+  await app.listen(envs.PORT);
+  logger.log(`Server running on port: ${envs.PORT}`);
 }
 bootstrap();
